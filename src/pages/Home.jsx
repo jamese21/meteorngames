4aedaf0b007ec5964b5549af7064f65f8631f210
jamese21 <thebigjtheawesome@gmail.com>
2024-08-02 20:38:47 +0900

    modified home page, not responsive yet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import portrait_banner from '../assets/portrait-banner.png'
+import meteorn_banner_wide from '../assets/meteorn-banner-wide.png'
 import './Home.css'
 
 function Home() {
   return (
     <div className="home-container">
-      <h1 className='main-title'>Meteorn Games</h1>
-      <p className='description'>
-        We are Meteorn Games. blablablablablablablabbla.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <img className="top-banner" src={meteorn_banner_wide}></img>
+      <h1 className="main-title">Meteorn Games</h1>
+      <div className="showcase">
+        {/* <img src={meteorn_banner} alt='meteorn_banner'></img> */}
+      </div>
+      <div className="list-container">
+        <div className="list-item">
+          <a href='https://meteornrun.io/' target='_blank'>
+            <img className="game-img" src={portrait_banner} />      
+          </a>
+        </div>
+        <div className="list-item">
+          <a href='https://meteornrun.io/' target='_blank'>
+            <img className="game-img" src={portrait_banner} />      
+          </a>
+        </div>
+        <div className="list-item">
+          <a href='https://meteornrun.io/' target='_blank'>
+            <img className="game-img" src={portrait_banner} />      
+          </a>
+        </div>
+        <div className="list-item">
+          <a href='https://meteornrun.io/' target='_blank'>
+            <img className="game-img" src={portrait_banner} />      
+          </a>
+        </div>
+      </div>
+      <button className="game-button">
+        View all games
+      </button>
     </div>
   )
 }
