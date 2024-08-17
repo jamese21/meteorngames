63eae96870b2f9fceabbf8a2961fdfa13b1da8d6
jamese21 <thebigjtheawesome@gmail.com>
2024-08-18 02:53:46 +0900

    Carousel autoscroll

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "./Slider.css"
 import {FaChevronRight, FaChevronLeft} from "react-icons/fa";
-import meteorn_banner from "../../assets/meteorn-banner.png"
+import meteorn_banner from "../../assets/meteorn-banner-wide.png"
 
 function Slider() {
+
+  useEffect(() => {
+    const radios = document.querySelectorAll('input[type="radio"]');
+    let currentIndex = 0;
+
+    const interval = setInterval(function() {
+      radios[currentIndex].checked = false;  // Uncheck the current radio button
+      currentIndex = (currentIndex + 1) % radios.length;  // Move to the next radio button
+      radios[currentIndex].checked = true;  // Check the next radio button
+    }, 5000);  // 5000ms = 5 seconds
+
+    return () => clearInterval(interval);  // Cleanup interval on component unmount
+  }, []);
+
   return (
     <div id="slider">
         <input type="radio" name="slider" id="slide1" defaultChecked/>
