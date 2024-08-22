c44f177e1c5ef294c8f4e0c0f15627d2c49f2273
jamese21 <thebigjtheawesome@gmail.com>
2024-08-22 18:00:08 +0900

    carousel responsive fix and search bar for games added

---
+++ src/games.js
@@ -0,0 +1,6 @@
+export const game_data = [{
+    "key": "meteorn-run",
+    "title": "meteorn run",
+    "link": "https://meteornrun.io/",
+    "banner": "meteorn-banner.png"
+}]
--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,31 +1,51 @@
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
 import meteorn_banner from '../assets/meteorn-banner.png'
 import './Games.css'
 import { useTranslation } from 'react-i18next'
+import {game_data} from "../games"
 
 function Games() {
 
   const [t, i18n] = useTranslation("global")
 
+  const [searchQuery, setSearchQuery] = useState("")
+
+  console.log(searchQuery)
+
   return (
     <div className="main-contents">
+
+      <input className="search-box" onChange={(e) => setSearchQuery(e.target.value)} 
+      placeholder={t("games.search-placeholder")} />
+
       <div className="games-grid">
-        <div className="game-container">
-          <img src={meteorn_banner} alt="game banner" className="profile-img"></img>
-          <h2 className="title">{t("games.meteorn-run-title")}</h2>
-          <a href='https://meteornrun.io/' target='_blank'>
-            <button className="btn">{t("games.play-button")}</button>        
-          </a>  
-        </div>
-        <div className="game-container coming-soon">
-          <h1>{t("games.coming-soon-label")}</h1>    
-        </div>
-        <div className="game-container coming-soon">
-          <h1>{t("games.coming-soon-label")}</h1>    
-        </div>
-        <div className="game-container coming-soon">
-          <h1>{t("games.coming-soon-label")}</h1>    
-        </div>
+        {game_data.filter((game) => {
+          return searchQuery.toLowerCase() === "" 
+          ? game 
+          : game.title.toLowerCase().includes(searchQuery.toLowerCase())
+        }).map((game) => (
+          <div key={game.key} className="game-container">
+            <img src={"/src/assets/".concat(game.banner)} alt="game banner" className="profile-img"></img>
+            <h2 className="title">{t("games.game-titles.".concat(game.key).concat("-title"))}</h2>
+            <a href={game.link} target='_blank'>
+              <button className="btn">{t("games.play-button")}</button>        
+            </a>  
+          </div>
+        ))}
+        {searchQuery === "" &&
+            <>
+              <div className="game-container coming-soon">
+                <h1>{t("games.coming-soon-label")}</h1>    
+              </div>
+              <div className="game-container coming-soon">
+                <h1>{t("games.coming-soon-label")}</h1>    
+              </div>
+              <div className="game-container coming-soon">
+                <h1>{t("games.coming-soon-label")}</h1>    
+              </div>
+            </>
+        }
+        
       </div>
     </div>
   )
